127d4a51a3005f3493c32745b2d91a3a6551b794
Smaran <smaran@123gmail.com>
2026-08-05 21:24:22 +0545

    Add user model, API error and response classes; implement CORS and cookie parsing middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({
   path: "./.env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed !!!", error);
+  });
