27e358043fe6634d4ff54c21eb96c3c43ffb472b
Smaran <smaran@123gmail.com>
2026-08-19 15:51:18 +0545

    feat: Update email transporter configuration for improved security and reliability

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -2,7 +2,11 @@ import nodemailer from "nodemailer";
 
 // Create a reusable transporter for sending emails.
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
