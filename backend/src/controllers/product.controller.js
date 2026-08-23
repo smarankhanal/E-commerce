90369bb36d9e97202004f018865c21ff38b88670
Smaran <smaran@123gmail.com>
2026-08-23 21:36:20 +0545

    feat: Add best sellers endpoint to product routes and controller

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -12,4 +12,28 @@ const getSingleProduct = asyncHandler(async (req, res) => {
   const product = await Product.find({ sku }).populate("collections", "name");
   return res.status(200).json(new ApiResponse(200, product, "Product fetched successfully"));
 });
-export { getAllProduct, getSingleProduct };
+const getBestSellers = asyncHandler(async (req, res) => {
+  const bestSellers = await Product.find({
+    isActive: true,
+    soldCount: { $gt: 0 },
+  })
+    .sort({ soldCount: -1 })
+    .limit(4)
+    .populate("collections", "name");
+
+  if (bestSellers.length === 0) {
+    const products = await Product.find({
+      isActive: true,
+    })
+      .limit(3)
+      .populate("collections", "name");
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, products, "Best sellers fetched successfully"));
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, bestSellers, "Best sellers fetched successfully"));
+});
+export { getAllProduct, getSingleProduct, getBestSellers };
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { getAllProduct, getSingleProduct } from "../controllers/product.controller.js";
+import {
+  getAllProduct,
+  getBestSellers,
+  getSingleProduct,
+} from "../controllers/product.controller.js";
 const router = Router();
 router.route("/").get(getAllProduct);
+router.route("/best-sellers").get(getBestSellers);
 router.route("/:sku").get(getSingleProduct);
 export default router;
