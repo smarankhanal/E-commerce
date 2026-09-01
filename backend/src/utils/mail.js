af32c8d75f7d4632a120c27cf4f8e9f7da6f538c
Smaran <smaran@123gmail.com>
2026-09-01 19:51:19 +0545

    fix: Update email transporter configuration to use SMTP instead of Gmail service

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -2,7 +2,14 @@ import nodemailer from "nodemailer";
 
 // Create a reusable transporter for sending emails.
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  // service: "gmail",
+  // auth: {
+  //   user: process.env.EMAIL_USER,
+  //   pass: process.env.EMAIL_PASS,
+  // },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
