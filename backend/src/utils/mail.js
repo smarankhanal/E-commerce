66e8ea88c27db5c0a8439f0f8d5355afa0faf005
Smaran <smaran@123gmail.com>
2026-09-01 20:02:11 +0545

    fix: Update email transporter configuration to require TLS

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -2,15 +2,10 @@ import nodemailer from "nodemailer";
 
 // Create a reusable transporter for sending emails.
 const transporter = nodemailer.createTransport({
-  // service: "gmail",
-  // auth: {
-  //   user: process.env.EMAIL_USER,
-  //   pass: process.env.EMAIL_PASS,
-  // },
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
